c93c3589b42301b4e44f2c1dea5559a1cb28126a
Nerme Rojas <nerme.rojas@studio.unibo.it>
2026-03-22 14:29:46 +0100

    Esempio ciclo while

--- Iterazione/while/esWhile.java
+++ Iterazione/while/esWhile.java
@@ -3,8 +3,8 @@ public class esWhile {
     public static void main(String[] args) {
 
         int i = 1;
-        System.out.println("Inizio ciclo");
 
+        System.out.println("Inizio ciclo");
         while (i <= 10) {
             System.out.print(i + " ");
             i = i + 1;
