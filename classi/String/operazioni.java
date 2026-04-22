699f911ecb89dfe40f9f74af78931d086fc55888
Nerme Rojas <nerme.rojas@studio.unibo.it>
2026-04-22 18:08:39 +0200

    Operazioni con string

--- classi/String/operazioni.java
+++ classi/String/operazioni.java
@@ -21,6 +21,26 @@ public class operazioni{
         System.out.println(parola.toUpperCase());
         // Converte tutti i caratteri in maiuscolo
 
+System.out.println("------------------");
+
+        String a = "ora é";
+        String b = "momento";
+        String c = " il";
+
+        System.out.println(a.length());
+        System.out.println(a.charAt(4));
+        System.out.println(a.substring(0,3));
+        System.out.println(a.length());
+        System.out.println(b.startsWith("mom"));
+        System.out.println(b.indexOf("mento"));
+        System.out.println(a.concat(c));
+        System.out.println(b.toUpperCase());
+
+        String[] risultato = a.split(" ");
+        System.out.println(risultato[0]);
+        System.out.println(risultato[1]);
+
+        System.out.println(a.compareTo(b));
 
     }
 
