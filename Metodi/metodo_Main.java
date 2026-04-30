bd7c9a063ceb29ab5270a506a8f01afc59f28831
Nerme Rojas <nerme.rojas@studio.unibo.it>
2026-04-30 13:39:05 +0200

    Diverse modifiche

--- Metodi/metodo_Main.java
+++ Metodi/metodo_Main.java
@@ -8,3 +8,13 @@ public class metodo_Main {
             System.out.println(a); // !! Per trasformarle in interi, si può usare Integer.parseInt
     }
 }
+/*
+Il main è un metodo che non ritorna un valore
+– O meglio, ritorna un valore di tipo void
+– Altri esempi: println(), set(i,x), clear(), …
+• I metodi void hanno tipicamente effetti collaterali (side-
+effects), ad es. modificano oggetti, file, database…
+• Per un metodo void la keyword return è opzionale:
+– Se c’è, si usa solo return; (non return void;)
+– Se non c’è, ritorna dopo aver eseguito il corpo
+*/
